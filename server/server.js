60eadf935decb20b856037e389660070f3fd7815
Smitraj <58396188+ByteNinjaSmit@users.noreply.github.com>
2024-02-29 20:25:20 +0530

    contact Form

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const app= express();
-const router = require('./router/auth-router');
-const connectDb =require("./utils/db");
+const app = express();
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
+const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 app.use(express.json());
 
-app.use("/api/auth",router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+
 app.use(errorMiddleware);
 
-const PORT =5000;
+const PORT = 5000;
 
-connectDb().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server is running at port: ${PORT}`);
-    });
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
 });
