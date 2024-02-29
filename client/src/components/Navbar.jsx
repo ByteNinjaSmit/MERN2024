5ed632eabf99ffea17e1101f5acd12b3aa4cafbe
Smitraj <58396188+ByteNinjaSmit@users.noreply.github.com>
2024-03-01 00:17:46 +0530

    registration page UI Done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,45 +14,70 @@ const ColorSchemesExample = () => {
     <>
       <Navbar expand="lg" className="bg-dark navbar-dark ">
         <Container fluid>
-          <Navbar.Brand to="/">MERN_2024</Navbar.Brand>
+          <Navbar.Brand style={{ color: 'mediumpurple', fontSize:'25px' }} to="/">MERN_2024</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0 justify-content-evenly"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Link
-                style={{ color: location.pathname === '/' ? 'gray' : 'white', textDecoration: 'none', padding: '5px 12px' }}
-                to="/"
-              >
-                Home
-              </Link>
-              <Link
-                style={{ color: location.pathname === '/about' ? 'gray' : 'white', textDecoration: 'none', padding: '5px 12px' }}
-                to="/about"
-              >
-                About
-              </Link>
-              <Link
-                style={{ color: location.pathname === '/service' ? 'gray' : 'white', textDecoration: 'none', padding: '5px 12px' }}
-                to="/service"
-              >
-                Serivce
-              </Link>
-              <Link
-                style={{ color: location.pathname === '/contact' ? 'gray' : 'white', textDecoration: 'none', padding: '5px 12px' }}
-                to="/contact"
-              >
-                Contact
-              </Link>
-              <Link
-                style={{ color: location.pathname === '/register' ? 'gray' : 'white', textDecoration: 'none', padding: '5px 12px' }}
-                to="/register"
-              >
-                Register
-              </Link>
-            </Nav>
+          <Nav
+  className="me-auto my-2 my-lg-0 justify-content-evenly"
+  style={{ maxHeight: '100px' }}
+  navbarScroll
+>
+  <Link
+    style={{
+      fontSize: location.pathname === '/' ? '16px' : 'white',
+      color: location.pathname === '/' ? 'gray' : 'white',
+      textDecoration: 'none',
+      padding: '5px 12px',
+    }}
+    to="/"
+  >
+    Home
+  </Link>
+  <Link
+    style={{
+      fontSize: location.pathname === '/about' ? '16px' : 'white',
+      color: location.pathname === '/about' ? 'gray' : 'white',
+      textDecoration: 'none',
+      padding: '5px 12px',
+    }}
+    to="/about"
+  >
+    About
+  </Link>
+  <Link
+    style={{
+      fontSize: location.pathname === '/service' ? '16px' : 'white',
+      color: location.pathname === '/service' ? 'gray' : 'white',
+      textDecoration: 'none',
+      padding: '5px 12px',
+    }}
+    to="/service"
+  >
+    Serivce
+  </Link>
+  <Link
+    style={{
+      fontSize: location.pathname === '/contact' ? '16px' : 'white',
+      color: location.pathname === '/contact' ? 'gray' : 'white',
+      textDecoration: 'none',
+      padding: '5px 12px',
+    }}
+    to="/contact"
+  >
+    Contact
+  </Link>
+  <Link
+    style={{
+      fontSize: location.pathname === '/register' ? '16px' : 'white',
+      color: location.pathname === '/register' ? 'gray' : 'white',
+      textDecoration: 'none',
+      padding: '5px 12px',
+    }}
+    to="/register"
+  >
+    Register
+  </Link>
+</Nav>
             <Link to="/Login">
               <Button variant="success">Login</Button>
             </Link>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Register = () => {
-  return (
-    <div>
-      <h1>This is Register page</h1>
-    </div>
-  )
-}
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  // handle input user value
+  const handleInput = (e) => {
+    console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({
+      ...user,
+      [name]:value,
+    });
+  };
 
-export default Register
+  // handeling form submit
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(user);
+  }
+
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <div className="registration-image"></div>
+          </Col>
+          <Col>
+            <h1 className="mt-3 mb-1 ">Registration Pgae</h1>
+            <Form className="mt-3" onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="username"
+                  name="username"
+                  value={user.username}
+                  placeholder="Enter Username"
+                  required
+                  autocomplete="off"
+                  onChange={handleInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={user.email}
+                  placeholder="Enter email"
+                  required
+                  autocomplete="off"
+                  onChange={handleInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type="phone"
+                  name="phone"
+                  value={user.phone}
+                  placeholder="Phone Number"
+                  required
+                  autocomplete="off"
+                  onChange={handleInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={user.password}
+                  placeholder="Password"
+                  required
+                  autocomplete="off"
+                  onChange={handleInput}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="success" type="submit" className="btn mx-2">
+                Register
+              </Button>
+              <Button variant="secondary" type="reset" className="btn mx-2">
+                Reset
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Register;
