e6f23f59be2d958d370bec657435b229c1661c64
Smitraj <58396188+ByteNinjaSmit@users.noreply.github.com>
2024-03-01 22:56:34 +0530

    user-data jwt verify

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -75,4 +75,18 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { home, register, login };
+// *--------------------------
+// to send User data Logic
+// *--------------------------
+
+const user = async (req, res) => {
+  try {
+    const userData = req.user;
+    console.log(userData);
+    return res.status(200).json({ msg: userData });
+  } catch (error) {
+    console.log(`error from the user route ${error}`);
+  }
+};
+
+module.exports = { home, register, login, user };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
 const { signupSchema, logininSchema } = require("../validators/auth-validator");
+const authMiddleware = require("../middlewares/auth-middleware");
+
 
 const validate = require("../middlewares/validate-middleware");
 
@@ -10,5 +12,6 @@ router
   .route("/register")
   .post(validate(signupSchema), authcontrollers.register);
 router.route("/login").post(validate(logininSchema), authcontrollers.login);
+router.route("/user").get(authMiddleware,authcontrollers.user);
 
 module.exports = router;
